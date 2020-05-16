7b0621930045a10bad46c3f576f541e7f05009f3
Rayti <milosz.karlowicz@gmail.com>
2020-05-16 11:24:00 +0200

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Bye Bye!" << std::endl;
     return 0;
 }
