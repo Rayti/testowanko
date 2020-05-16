8ccb6c512d65bc75e7d5b961fa353337843fe2c5
jakubiecsm <jakubiecsm@gmail.com>
2020-05-16 12:07:35 +0200

    elo1

--- main.cpp
+++ main.cpp
@@ -3,5 +3,7 @@
 int main() {
     std::cout << "Hello World!" << std::endl;
     std::cout << "Bye Baaye!" << std::endl;
+    std::cout << "elo" << std::endl;
+    std::cout << "asdfsdf";
     return 0;
 }
